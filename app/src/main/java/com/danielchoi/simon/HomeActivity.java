ba849ba69ace142ad9bceee199be74a7ee0ec3d6
Daniel <dchoi3@my.apsu.edu>
2017-02-16 19:27:39 -0600

    Switched to correct activity

--- app/src/main/java/com/danielchoi/simon/HomeActivity.java
+++ app/src/main/java/com/danielchoi/simon/HomeActivity.java
@@ -1,5 +1,8 @@
 package com.danielchoi.simon;
 
+import android.content.Context;
+import android.content.Intent;
+import android.os.Vibrator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,89 +12,30 @@ import android.widget.ImageButton;
 import java.util.Random;
 import java.util.Vector;
 
-public class HomeActivity extends AppCompatActivity
-implements View.OnClickListener{
-    // Private Global Values
-    private int count = 1;
-    Vector<Integer> userPattern = new Vector<>();
-    // Color button ids
-    private int[] colors = new int[]{R.id.blue_imageButton, R.id.green_imageButton, R.id.red_imageButton, R.id.yellow_imageButton};
+public class HomeActivity extends AppCompatActivity{
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_game);
+        setContentView(R.layout.activity_home);
 
-        Button b = (Button) findViewById(R.id.play_button);
-            b.setOnClickListener(this);
-        for(int id : colors){
-            ImageButton ib = (ImageButton) findViewById(id);
-            ib.setOnClickListener(this);
-        }
+        ImageButton ib = (ImageButton) findViewById(R.id.start_imageButton);
+        if(ib != null){
+            ib.setOnClickListener(new View.OnClickListener(){
 
-        simonsTurn();
-        usersTurn();
-    }
-    // Function that shows simon's pattern for the user to repeat
-    private void simonsTurn() {
-        lockButtons(); // Stop user from clicking buttons until simon is finished
-        // Call Random Color ID
-        int colorId = setPattern();
-        Vector<Integer> simonPattern = new Vector<>();
-        simonPattern.add(colorId);
-        unLockButtons();
-    }
-    // Function that allows user to repeat simon's pattern
-    private void usersTurn(){
-        // Compare user pattern to simon's
-
-    }
-    // Function that makes simon choose random colors
-    private int setPattern(){
-        int id = R.id.blue_imageButton;
-        Random rand = new Random();
-        int index = rand.nextInt(5);
-        for(int i=0; i<count; i++){
-            if(i == index){
-                colors[i] = id;
-            }
-        }
-        count++;
-        return id;
-    }
-    // Function to stop user from clicking buttons while simon is active
-    private void lockButtons(){
-        for(int id : colors){
-            ImageButton ib = (ImageButton) findViewById(id);
-            ib.setClickable(false);
-            ib.setLongClickable(false);
-        }
-    }
-    // Function to let user click buttons when simon is inactive
-    private void unLockButtons(){
-        for(int id : colors){
-            ImageButton ib = (ImageButton) findViewById(id);
-            ib.setClickable(true);
-            ib.setLongClickable(false);
-        }
-    }
-
-    @Override
-    public void onClick(View view) {
-        if(view.getId() == R.id.play_button){
-            simonsTurn();
-
-        }
-        // Adds a color to the users pattern until count matches simon
-        for(int i=0; i<count; i++) {
-            for (int ids : colors) {
-                if (view.getId() == ids){
-                    int colorId = view.getId();
-                    userPattern.add(colorId);
-                    usersTurn();
+                @Override
+                public void onClick(View v){
+                    Vibrator vb = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+                    vb.vibrate(10);
+                    Intent startIntent = new Intent(getApplicationContext(), GameActivity.class);
+                    startActivity(startIntent);
                 }
-            }
+            });
         }
 
 
+
+
     }
+
 }
