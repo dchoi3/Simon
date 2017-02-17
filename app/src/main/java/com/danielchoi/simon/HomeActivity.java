79d7250352f5eecc0cfa59140c129f7e3da9c85f
jschmitt4 <jschmitt4@my.apsu.edu>
2017-02-16 18:27:20 -0600

    Raw code to start simon

--- app/src/main/java/com/danielchoi/simon/HomeActivity.java
+++ app/src/main/java/com/danielchoi/simon/HomeActivity.java
@@ -2,35 +2,52 @@ package com.danielchoi.simon;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import java.util.Random;
 import java.util.Vector;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity
+implements View.OnClickListener{
     // Private Global Values
     private int count = 1;
-    // Use this When Layout is finished:
-    // private int[] colors = new int[]{R.id.blue, R.id.green, R.id.red, R.id.yellow};
-    private int[] colors = new int[]{R.id.textView, R.id.textView, R.id.textView, R.id.textView};
+    Vector<Integer> userPattern = new Vector<>();
+    // Color button ids
+    private int[] colors = new int[]{R.id.blue_imageButton, R.id.green_imageButton, R.id.red_imageButton, R.id.yellow_imageButton};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_game);
 
+        Button b = (Button) findViewById(R.id.play_button);
+            b.setOnClickListener(this);
+        for(int id : colors){
+            ImageButton ib = (ImageButton) findViewById(id);
+            ib.setOnClickListener(this);
+        }
 
-        Vector<Integer> userPattern = new Vector<>();
-        displaySimon();
+        simonsTurn();
+        usersTurn();
     }
-
-    private void displaySimon() {
+    // Function that shows simon's pattern for the user to repeat
+    private void simonsTurn() {
+        lockButtons(); // Stop user from clicking buttons until simon is finished
         // Call Random Color ID
         int colorId = setPattern();
         Vector<Integer> simonPattern = new Vector<>();
         simonPattern.add(colorId);
+        unLockButtons();
+    }
+    // Function that allows user to repeat simon's pattern
+    private void usersTurn(){
+        // Compare user pattern to simon's
 
     }
+    // Function that makes simon choose random colors
     private int setPattern(){
-        int id = R.id.textView; // Change after layout is complete
+        int id = R.id.blue_imageButton;
         Random rand = new Random();
         int index = rand.nextInt(5);
         for(int i=0; i<count; i++){
@@ -40,5 +57,41 @@ public class HomeActivity extends AppCompatActivity {
         }
         count++;
         return id;
+    }
+    // Function to stop user from clicking buttons while simon is active
+    private void lockButtons(){
+        for(int id : colors){
+            ImageButton ib = (ImageButton) findViewById(id);
+            ib.setClickable(false);
+            ib.setLongClickable(false);
+        }
+    }
+    // Function to let user click buttons when simon is inactive
+    private void unLockButtons(){
+        for(int id : colors){
+            ImageButton ib = (ImageButton) findViewById(id);
+            ib.setClickable(true);
+            ib.setLongClickable(false);
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view.getId() == R.id.play_button){
+            simonsTurn();
+
+        }
+        // Adds a color to the users pattern until count matches simon
+        for(int i=0; i<count; i++) {
+            for (int ids : colors) {
+                if (view.getId() == ids){
+                    int colorId = view.getId();
+                    userPattern.add(colorId);
+                    usersTurn();
+                }
+            }
+        }
+
+
     }
 }
